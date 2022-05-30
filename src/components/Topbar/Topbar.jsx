b93125d2aa9b32f95a31e2d9c1e57f754ecb4e56
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-05-30 02:52:36 +0200

    add : Context hook to maintain Cart items

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { Navbar,Container,Nav,NavDropdown, Form,Button,FormControl,Badge} from 'react-bootstrap';
-import './Topbar.css';
+import {React,useContext,useState} from 'react'
+import { Navbar,Container,Nav,Row, Form,Button,FormControl,Badge} from 'react-bootstrap';
+import UserContext from '../Context/Context';
+import ShoppingCart from '../ShoppingCart/ShoppingCart';
+
 function Topbar() {
+  const [cart_val,setCartVal] = useContext(UserContext);
+  const [modalShow,setModalShow] = useState(false);
   return (
+    <Row>
     <Navbar bg="light" expand="lg">
     <Container fluid>
-    <Navbar.Brand href="#home">
+    <Navbar.Brand href="" >
         <img
           alt=""
           src="/zooPoints.svg"
@@ -14,12 +19,12 @@ function Topbar() {
           className="d-inline-block align-top"
         />{' '}
        Zooplus
-      </Navbar.Brand>
+      </Navbar.Brand >
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
         className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
+        style={{ maxHeight: '300px' }}
         navbarScroll
       >
         <Form className="d-flex">
@@ -32,17 +37,21 @@ function Topbar() {
         <Button variant="outline-success">Search</Button>
       </Form>
       </Nav>
-      <Nav.Link >Login</Nav.Link>
-      <Button variant="success">
-  Shopping Cart <Badge bg="secondary"> 9</Badge>
+      <Button onClick={() => setModalShow(true)} variant="success">
+  Shopping Cart <Badge bg="secondary"> {cart_val}</Badge>
   <span className="visually-hidden">unread messages</span>
 </Button>
 
-    </Navbar.Collapse>
-    
+  </Navbar.Collapse>
   </Container>
- 
 </Navbar>
+<ShoppingCart
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+
+</Row>
+
   )
 }
 

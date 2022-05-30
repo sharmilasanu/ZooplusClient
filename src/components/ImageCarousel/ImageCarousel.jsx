468aeacc251bd987e5e93bc34b721080b02a544d
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-05-30 20:51:04 +0200

    add : Image carousel for  Pproductview page

---
+++ src/components/ImageCarousel/ImageCarousel.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import Carousel from 'react-bootstrap/Carousel'
+const ImageCarousel = () => {
+    return (
+<Carousel variant="dark">
+  <Carousel.Item>
+    <img
+      className="d-block w-30"
+      src="https://uxt-cf-images.mediazs.com/1vo7yhprbsja/2YidAVNym4YnxQ8SWcWl0a/5e0148121c7283b94750ec17f26ae87f/2021_09_Cosma_Snackies_Maxitube_Offer_V2_1000x320_DE.jpg?fm=jpg&fl=progressive&w=725&q=85"
+      alt=""
+    />
+    <Carousel.Caption>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+    
+      className="d-block w-30"
+      src="https://uxt-cf-images.mediazs.com/1vo7yhprbsja/2229vikJDcBosZPAd0qIKC/44f5f60a61b8599528b09b86040525d0/2021_03_Lukullus_General_Freebie_1000x320_DE.jpg?fm=jpg&fl=progressive&w=725&q=85"
+      alt=""
+    />
+    <Carousel.Caption>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-30"
+      src="https://uxt-cf-images.mediazs.com/1vo7yhprbsja/maaYbUH8OtuhufBHrwsdp/ec6623f6e59870b6a3cb4daa89f1dd00/2022_05_Summer_General_1000x320_DE.jpg?fm=jpg&fl=progressive&w=725&q=85"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+    )
+}
+
+export default ImageCarousel

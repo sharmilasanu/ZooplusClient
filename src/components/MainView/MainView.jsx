5d8104e00a1b65032a601e2f43f9d0ee3a3917ca
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-05-30 02:55:41 +0200

    add : Usercontext Provider setup

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import Topbar from '../Topbar/Topbar';
-export default function MainView() {
+import HomePage from '../HomePage/HomePage';
+import ProductView from '../ProductView/ProductView';
+import UserContext from '../Context/Context';
+
+
+const MainView = () => {
+  const [cart_val,setCartVal] = useState(0); 
   return (
     <div className='main-view'>
-     <Topbar/>
+    <UserContext.Provider value= {[cart_val,setCartVal]}>
+    <Topbar/>
+    
+     <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route exact path="/product/:productId" element = {<ProductView/>}/>
+      </Routes>
+      </Router>
+      </UserContext.Provider>
     </div>
   )
+
 }
+
+export default MainView

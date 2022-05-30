a0ce7643abbc0b556e0439f90f5175af2b2ae7f8
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-05-30 20:53:43 +0200

    add : shopping cart  design Component

--- src/components/ShoppingCartMedia/ShoppingCartMedia.jsx
+++ src/components/ShoppingCartMedia/ShoppingCartMedia.jsx
@@ -1,40 +1,33 @@
-import React, { Component } from "react";
-import { Button, Row, Col,  Accordion } from "react-bootstrap";
-
-
+import {React,useState} from "react";
+import { Button, Row, Col, Container } from "react-bootstrap";
+import axios from "axios"
+import ShoppingCartDetails from '../ShoppingCartDetails/ShoppingCartDetails';
 function ShoppingCartMedia() {
-    return(
-    
-            <Row>
-              <img
-                width={100}
-                height={100}
-                className="align-self-center mr-3"
-                src=""
-                alt="Generic placeholder"
-              />
-              
-                <p>Dxracer Formula Gaming Chair (Black/Red)</p>
-                <Row>
-                  <Col xs={6}>
-                    <strong>$48.99</strong>
-                  </Col>
-                  <Col xs={6}>1 piece</Col>
-                </Row>
+  const [cartItems,setCartItems] = useState([]);
 
-                <Row>
-                  <Col xs={6}>
-                    <Button variant="primary" size="sm">
-                      Details
-                    </Button>
-                  </Col>
-                  <Col xs={6}>
-                    <Button variant="danger" size="sm">
-                      Delete
-                    </Button>
-                  </Col>
-                </Row>
-                </Row>
+  
+  const userID = "sharmila"
+  axios
+  .get('https://zooplusecomm.herokuapp.com/api/cart/find/'+ userID)
+  .then(
+    response => {
+      
+      console.log(response.data)
+      setCartItems(response.data)
+    })
+    .catch(err => { 
+      console.error(err)
+  });
+
+    return(
+       
+       <Container>
+           {cartItems.map(product => (
+            <Col key={product._id}>
+                <ShoppingCartDetails product={product} />
+            </Col> 
+            ))}
+       </Container>
             
     )
     
